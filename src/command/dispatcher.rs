67c7ed58d40519f7fd8e595588fa66518ef476a7
Saad <mohd.saadhaider@gmail.com>
2026-03-14 16:55:49 +0530

    fix: use db directly

--- src/command/dispatcher.rs
+++ src/command/dispatcher.rs
@@ -2,6 +2,7 @@ use anyhow::Error;
 
 use crate::command::command::Command;
 use crate::config::client::Client;
+use crate::database::core::RedisObject;
 use crate::resp::types::RespType;
 
 // Dispatch the commands (execute the command)
@@ -14,13 +15,13 @@ pub fn dispatch(client: Client, cmd: Command) -> Result<RespType, Error> {
             Ok(RespType::BulkString(message))
         }
         Command::Set(key, value) => {
-            match client.set(key, value) {
+            match client.db.set(key, RedisObject::String(value)) {
                 Ok(()) => Ok(RespType::SimpleString("Ok".to_string())),
                 Err(e) => Err(anyhow::anyhow!("Failed to execute command. E: {}", e)),
             }
         }
         Command::Get(key) => {
-            match client.get(key) {
+            match client.db.get(key) {
                 Ok(val) => Ok(RespType::BulkString(val)),
                 Err(e) => Err(anyhow::anyhow!("Failed to execute command. E: {}", e)),
             }
--- src/config/client.rs
+++ src/config/client.rs
@@ -1,21 +1,11 @@
-use anyhow::Error;
-
-use crate::database::core::{DB, RedisObject};
+use crate::database::core::DB;
 
 pub struct Client {
-    db: DB,
+    pub db: DB,
 }
 
 impl Client {
     pub fn new(db: DB) -> Client {
         Client { db }
-    }
-
-    pub fn set(&self, k: String, v: String) -> Result<(), Error> {
-        self.db.set(k, RedisObject::String(v))
-    }
-
-    pub fn get(&self, k: String) -> Result<String, Error> {
-        self.db.get(k)
     }
 }
